e4953ad6f53cce02a4249de6d09e43db429d04e7
Walter White <walterwhite@DPEMS-MBA920.lan>
2025-02-08 12:18:48 -0500

    project 4 final

--- scripts/index.js
+++ scripts/index.js
@@ -31,6 +31,12 @@ console.log(initialCards);
 let modal = document.querySelector(".modal");
 let editButton = document.querySelector(".profile__edit-button");
 let closeButton = document.querySelector(".modal__close");
+/* form javascript variables */
+let modalInputTitle = document.getElementById("modal__input_title");
+let modalInputDescription = document.getElementById("modal__input_description");
+let saveButton = document.querySelector(".modal__button");
+let profileTitle = document.querySelector(".profile__title");
+let profileDescription = document.querySelector(".profile__description");
 
 editButton.addEventListener("click", ()=> {
    
@@ -41,3 +47,16 @@ closeButton.addEventListener("click", ()=> {
   modal.classList.add("modal__close");
 })
 
+saveButton.addEventListener("click", (e)=>{
+  e.preventDefault();
+  const userTitle = modalInputTitle.value;
+  const userDescription = modalInputDescription.value;
+  if(userTitle.trim() && userDescription.trim()!== ""){
+    profileTitle.textContent = userTitle;
+    profileDescription.textContent = userDescription;
+    modal.classList.add("modal__close");
+  }
+
+  }
+
+)
